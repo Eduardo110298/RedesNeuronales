b3a62b9202ba93e7ff603cfadbc20a7d5899b919
Eduardo <eduardo_110298@hotmail.com>
2018-04-15 19:39:29 -0400

    valuable backup 4

--- PML/neural-network.py
+++ PML/neural-network.py
@@ -5,11 +5,6 @@ from numpy import exp, array, random
 
 class NeuralLayer():
     def __init__(self,number_of_neurons,number_of_inputs_per_neuron):
-        """ 
-        w Denote the weights matrix random at first.
-        n Denote the number of neurons in this layer.
-        u Denote a list with the threshold input in each neuron.
-        """
         self.w = list()
         self.w = 2 * random.random((number_of_neurons,number_of_inputs_per_neuron)) - 1 # Comment this line on tests.
         # self.w = array([[1]*number_of_inputs_per_neuron]*number_of_neurons) # Uncomment this line on tests (All weigths are 1).
@@ -22,31 +17,30 @@ class NeuralLayer():
         # self.u = [1]*number_of_neurons # Uncomment this line on tests (All thresholds are 1).
 
 class NeuralNetwork():
-    def __init__(self,input,layers,output,learning_rate):
+    def __init__(self,inputs,layers,outputs,learning_rate):
         self.w = list()
         self.w = [layer.w for layer in layers]
 
-        self.a = list()
-        self.a.append(input)
+        self.inputs = inputs
+        self.outputs = outputs
 
         self.u = list()
-        self.u.append([None]*len(input))
+        self.u.append([None]*len(inputs[0]))
         self.u += [layer.u for layer in layers]
 
         self.n = list()
-        self.n.append(len(input))
+        self.n.append(len(input[0]))
         self.n += [layer.n for layer in layers]
 
-        self.total_layers = len(layers)
+        self.total_layers = len(layers) + 1
+        self.learning_rate = learning_rate
+        
 
     def __sigmoid(self, x):
         return 1 / (1 + exp(-x))
 
     def think(self):
-        """
-        Generate the output.
-        """
-        for k in range(1,self.total_layers):
+        for k in range(1,self.total_layers-1):
             a = list()
             for i in range(0,self.n[k]):
                 x = int()
@@ -109,15 +103,48 @@ class NeuralNetwork():
 
         self.weight_deltas.append(layer3_delta)
 
-if __name__ == "__main__":
-    # Uncomment this lines only during tests:
+    def adjust_weights(self):
+        for k in range(0,self.total_layers-1):
+            for i in range(0,self.n[k+1]):
+                for j in range(0,self.n[k]):
+                    self.w[k][i,j] = self.w[k][i,j] - self.learning_rate * self.weight_deltas[k][i,j]
     
+    def weights_adjustment(self):
+        self.weight_deltas = list()
+        self.layer1_weight_deltas_calculus()
+        self.layer2_weight_deltas_calculus()
+        self.layer3_weight_deltas_calculus()
+        self.weight_deltas = array(self.weight_deltas)
+        self.adjust_weights()
+
+    def train(self,number_of_iterations):
+        for x in range(0,number_of_iterations):
+            for i in range(0,len(self.outputs)):
+                self.a = list()
+                self.a.append(inputs[i])
+                self.output = self.outputs[i]
+
+                self.think()
+                self.weights_adjustment()
+                # self.threshold_adjustment()
+
+if __name__ == "__main__":
+
+    inputs = [[0,0,0]]
+    layer1 = NeuralLayer(4,3)
+    layer2 = NeuralLayer(4,4)
+    layer3 = NeuralLayer(1,4)
+    outputs = [0]
+    learning_rate = 0.001
+
+    neuralNetwork = NeuralNetwork(inputs,[layer1,layer2,layer3],outputs,learning_rate)
+    neuralNetwork.train(60000)
+    
+    # Uncomment this lines only during tests:
     # layer1 = NeuralLayer(3,2)
     # output_layer = NeuralLayer(1,3)
-
     # neuralNetwork = NeuralNetwork([2,3],[layer1,output_layer],None,None)
     # neuralNetwork.think()
-
     # print(neuralNetwork.w)
     # print(neuralNetwork.u)
     # print(neuralNetwork.n)
