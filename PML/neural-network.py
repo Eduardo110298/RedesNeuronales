45b1f7ddfcaf421652ac7fd752cf1e0ff9121d49
Unknown <andreseloyluna@gmail.com>
2018-04-26 10:52:38 -0400

    debugging phase

--- PML/neural-network.py
+++ PML/neural-network.py
@@ -4,55 +4,55 @@
 from numpy import exp, array, random
 
 class NeuralLayer():
-    def __init__(self,number_of_neurons,number_of_inputs_per_neuron):
-        self.w = list()
-        self.w = 2 * random.random((number_of_neurons,number_of_inputs_per_neuron)) - 1 # Comment this line on tests.
-        # self.w = array([[1]*number_of_inputs_per_neuron]*number_of_neurons) # Uncomment this line on tests (All weigths are 1).
+    # def __init__(self,number_of_neurons,number_of_inputs_per_neuron):
+    #     self.w = list()
+    #     self.w = 2 * random.random((number_of_neurons,number_of_inputs_per_neuron)) - 1 # Comment this line on tests.
 
-        self.n = int()
-        self.n = number_of_neurons
+
+    #     self.n = int()
+    #     self.n = number_of_neurons
         
-        self.u = list()
-        self.u = random.random(number_of_neurons) # Comment this line on tests.
-        # self.u = [1]*number_of_neurons # Uncomment this line on tests (All thresholds are 1).
+    #     self.u = list()
+    #     self.u = random.random(number_of_neurons) # Comment this line on tests.
+
         
         # print("w",self.w)
         # print("n",self.n)
         # print("u",self.u)
 
 class NeuralNetwork():
-    def __init__(self,inputs,layers,outputs,learning_rate):
-        self.w = list()
-        self.w = [layer.w for layer in layers]
+    # def __init__(self,inputs,layers,outputs,learning_rate):
+    #     self.w = list()
+    #     self.w = [layer.w for layer in layers]
 
-        self.inputs = inputs
-        self.outputs = outputs
+    #     self.inputs = inputs
+    #     self.outputs = outputs
 
-        self.u = list()
-        self.u.append([None]*len(inputs[0]))
-        self.u += [layer.u for layer in layers]
+    #     self.u = list()
+    #     self.u.append([None]*len(inputs[0]))
+    #     self.u += [layer.u for layer in layers]
 
-        self.n = list()
-        self.n.append(len(inputs[0]))
-        self.n += [layer.n for layer in layers]
+    #     self.n = list()
+    #     self.n.append(len(inputs[0]))
+    #     self.n += [layer.n for layer in layers]
 
-        self.total_layers = len(layers) + 1
-        self.learning_rate = learning_rate
+    #     self.total_layers = len(layers) + 1
+    #     self.learning_rate = learning_rate
         
 
-    def __sigmoid(self, x):
-        return 1 / (1 + exp(-x))
+    # def __sigmoid(self, x):
+    #     return 1 / (1 + exp(-x))
 
-    def think(self):
-        for k in range(1,self.total_layers):
-            a = list()
-            for i in range(0,self.n[k]):
-                x = int()
-                for j in range(0,self.n[k-1]):
-                    x += self.w[k-1][i,j] * self.a[k-1][j]
-                x += self.u[k][i]
-                a.append(self.__sigmoid(x))
-            self.a.append(a)
+    # def think(self):
+    #     for k in range(1,self.total_layers):
+    #         a = list()
+    #         for i in range(0,self.n[k]):
+    #             x = int()
+    #             for j in range(0,self.n[k-1]):
+    #                 x += self.w[k-1][i,j] * self.a[k-1][j]
+    #             x += self.u[k][i]
+    #             a.append(self.__sigmoid(x))
+    #         self.a.append(a)
 
     def predict(self,input,output):
         self.input = input
@@ -61,11 +61,11 @@ class NeuralNetwork():
         self.think()
         print(self.a[self.total_layers-1])
 
-    def __sigmoid_derivative(self,x):
-        return x * (1 - x)
+    # def __sigmoid_derivative(self,x):
+    #     return x * (1 - x)
 
-    def __error_derivative(self,expected,given):
-        return - (expected - given)
+    # def __error_derivative(self,expected,given):
+    #     return - (expected - given)
 
     # def __layer3_sigma(self,i):
     #     # print("a:",self.a)
@@ -87,109 +87,109 @@ class NeuralNetwork():
 
     #     return self.__sigmoid_derivative(self.a[self.total_layers-3][k]) * layer1_ways_sum
 
-    def layer1_weight_deltas_calculus(self):
-        layer1_delta = list()
-        for k in range(0,self.n[self.total_layers-3]):
-            deltas = list()
-            for j in range(0,self.n[self.total_layers-4]):
-                delta = int()
-                delta = self.a[self.total_layers-4][j] * self.__layer1_sigma(k)
-                deltas.append(delta)
-            layer1_delta.append(deltas)
+    # def layer1_weight_deltas_calculus(self):
+    #     layer1_delta = list()
+    #     for k in range(0,self.n[self.total_layers-3]):
+    #         deltas = list()
+    #         for j in range(0,self.n[self.total_layers-4]):
+    #             delta = int()
+    #             delta = self.a[self.total_layers-4][j] * self.__layer1_sigma(k)
+    #             deltas.append(delta)
+    #         layer1_delta.append(deltas)
 
-        self.weight_deltas.append(array(layer1_delta))
+    #     self.weight_deltas.append(array(layer1_delta))
 
-    def layer2_weight_deltas_calculus(self):
-        layer2_delta = list()
-        for k in range(0,self.n[self.total_layers-2]):
-            deltas = list()
-            for j in range(0,self.n[self.total_layers-3]):
-                delta = int()
-                delta = self.a[self.total_layers-3][j] * self.__layer2_sigma(k)
-                deltas.append(delta)
-            layer2_delta.append(deltas)
+    # def layer2_weight_deltas_calculus(self):
+    #     layer2_delta = list()
+    #     for k in range(0,self.n[self.total_layers-2]):
+    #         deltas = list()
+    #         for j in range(0,self.n[self.total_layers-3]):
+    #             delta = int()
+    #             delta = self.a[self.total_layers-3][j] * self.__layer2_sigma(k)
+    #             deltas.append(delta)
+    #         layer2_delta.append(deltas)
 
-        self.weight_deltas.append(array(layer2_delta))
+    #     self.weight_deltas.append(array(layer2_delta))
 
-    def layer3_weight_deltas_calculus(self):
-        layer3_delta = list()
-        for i in range(0,self.n[self.total_layers-1]):
-            deltas = list()
-            for j in range(0,self.n[self.total_layers-2]):
-                delta = int()
-                delta = self.a[self.total_layers-2][j] * self.__layer3_sigma(i)
-                deltas.append(delta)
-            layer3_delta.append(deltas)
+    # def layer3_weight_deltas_calculus(self):
+    #     layer3_delta = list()
+    #     for i in range(0,self.n[self.total_layers-1]):
+    #         deltas = list()
+    #         for j in range(0,self.n[self.total_layers-2]):
+    #             delta = int()
+    #             delta = self.a[self.total_layers-2][j] * self.__layer3_sigma(i)
+    #             deltas.append(delta)
+    #         layer3_delta.append(deltas)
 
-        self.weight_deltas.append(array(layer3_delta))
+    #     self.weight_deltas.append(array(layer3_delta))
 
-    def adjust_weights(self):
-        # print("w:",self.w)
-        # print("weight_deltas:",self.weight_deltas)
-        for k in range(0,self.total_layers-1):
-            for i in range(0,self.n[k+1]):
-                for j in range(0,self.n[k]):
-                    self.w[k][i,j] = self.w[k][i,j] - self.learning_rate * self.weight_deltas[k][i,j]
+    # def adjust_weights(self):
+    #     # print("w:",self.w)
+    #     # print("weight_deltas:",self.weight_deltas)
+    #     for k in range(0,self.total_layers-1):
+    #         for i in range(0,self.n[k+1]):
+    #             for j in range(0,self.n[k]):
+    #                 self.w[k][i,j] = self.w[k][i,j] - self.learning_rate * self.weight_deltas[k][i,j]
     
-    def weights_adjustment(self):
-        self.weight_deltas = list()
-        self.layer1_weight_deltas_calculus()
-        self.layer2_weight_deltas_calculus()
-        self.layer3_weight_deltas_calculus()
-        self.weight_deltas = array(self.weight_deltas)
-        self.adjust_weights()
+    # def weights_adjustment(self):
+    #     self.weight_deltas = list()
+    #     self.layer1_weight_deltas_calculus()
+    #     self.layer2_weight_deltas_calculus()
+    #     self.layer3_weight_deltas_calculus()
+    #     self.weight_deltas = array(self.weight_deltas)
+    #     self.adjust_weights()
 
-    def layer1_threshold_deltas_calculus(self):
-        layer1_threshold = list()
-        for i in range(0,self.n[self.total_layers-3]):
-            delta = int()
-            delta = self.__layer1_sigma(i)
-            layer1_threshold.append(delta)
-        self.threshold_deltas.append(layer1_threshold)
+    # def layer1_threshold_deltas_calculus(self):
+    #     layer1_threshold = list()
+    #     for i in range(0,self.n[self.total_layers-3]):
+    #         delta = int()
+    #         delta = self.__layer1_sigma(i)
+    #         layer1_threshold.append(delta)
+    #     self.threshold_deltas.append(layer1_threshold)
 
-    def layer2_threshold_deltas_calculus(self):
-        layer2_threshold = list()
-        for i in range(0,self.n[self.total_layers-2]):
-            delta = int()
-            delta = self.__layer2_sigma(i)
-            layer2_threshold.append(delta)
-        self.threshold_deltas.append(layer2_threshold)
+    # def layer2_threshold_deltas_calculus(self):
+    #     layer2_threshold = list()
+    #     for i in range(0,self.n[self.total_layers-2]):
+    #         delta = int()
+    #         delta = self.__layer2_sigma(i)
+    #         layer2_threshold.append(delta)
+    #     self.threshold_deltas.append(layer2_threshold)
 
-    def layer3_threshold_deltas_calculus(self):
-        layer3_threshold = list()
-        for i in range(0,self.n[self.total_layers-1]):
-            delta = int()
-            delta = self.__layer3_sigma(i)
-            layer3_threshold.append(delta)
-        self.threshold_deltas.append(layer3_threshold)
+    # def layer3_threshold_deltas_calculus(self):
+    #     layer3_threshold = list()
+    #     for i in range(0,self.n[self.total_layers-1]):
+    #         delta = int()
+    #         delta = self.__layer3_sigma(i)
+    #         layer3_threshold.append(delta)
+    #     self.threshold_deltas.append(layer3_threshold)
         
-    def adjust_thresholds(self):
-        # print("u:",self.u)
-        # print("threshold_deltas:",self.threshold_deltas)
-        for i in range(1,self.total_layers):
-            for j in range(0,self.n[i]):
-                # print("position: (",i,j,")")
-                self.u[i][j] = self.u[i][j] - self.learning_rate * self.threshold_deltas[i][j]
+    # def adjust_thresholds(self):
+    #     # print("u:",self.u)
+    #     # print("threshold_deltas:",self.threshold_deltas)
+    #     for i in range(1,self.total_layers):
+    #         for j in range(0,self.n[i]):
+    #             # print("position: (",i,j,")")
+    #             self.u[i][j] = self.u[i][j] - self.learning_rate * self.threshold_deltas[i][j]
 
-    def thresholds_adjustment(self):
-        self.threshold_deltas = list()
-        self.layer0_threshold_deltas = [None]*self.n[0]
-        self.threshold_deltas.append(self.layer0_threshold_deltas)
-        self.layer1_threshold_deltas_calculus()
-        self.layer2_threshold_deltas_calculus()
-        self.layer3_threshold_deltas_calculus()
-        self.adjust_thresholds()
+    # def thresholds_adjustment(self):
+    #     self.threshold_deltas = list()
+    #     self.layer0_threshold_deltas = [None]*self.n[0]
+    #     self.threshold_deltas.append(self.layer0_threshold_deltas)
+    #     self.layer1_threshold_deltas_calculus()
+    #     self.layer2_threshold_deltas_calculus()
+    #     self.layer3_threshold_deltas_calculus()
+    #     self.adjust_thresholds()
 
-    def train(self,number_of_iterations):
-        for x in range(0,number_of_iterations):
-            for i in range(0,len(self.outputs)):
-                self.a = list()
-                self.a.append(self.inputs[i])
-                self.output = self.outputs[i]
+    # def train(self,number_of_iterations):
+    #     for x in range(0,number_of_iterations):
+    #         for i in range(0,len(self.outputs)):
+    #             self.a = list()
+    #             self.a.append(self.inputs[i])
+    #             self.output = self.outputs[i]
 
-                self.think()
-                self.weights_adjustment()
-                self.thresholds_adjustment()
+    #             self.think()
+    #             self.weights_adjustment()
+    #             self.thresholds_adjustment()
 
 if __name__ == "__main__":
 
--- PML/neural-network2.0.py
+++ PML/neural-network2.0.py
@@ -6,6 +6,12 @@ from math import exp
 
 class NeuralNetwork():
     def __init__(self, layers_sizes, learning_rate):
+        """
+        layer_sizes: List of number of neurons in each hidden layer.
+        len(layer_sizes) indicate how many hidden layers are in the network.
+        
+        learning_rate: the learning rate.
+        """
         self.n_layers = len(layers_sizes) + 2 # + 2 for input and output layers.
         self.layers_sizes = layers_sizes
         self.learning_rate = learning_rate
@@ -22,8 +28,20 @@ class NeuralNetwork():
     def __layer_sigma(self,layer,i):
         if (layer == self.n_layers - 1):
             return self.__sigmoid_derivative(self.a[layer][i]) * self.__error_derivative(self.output[i],self.a[layer][i])
-        else
+        else:
             return self.__sigmoid_derivative(self.a[layer][i]) * sum([self.w[layer][p][i] * self.__layer_sigma(layer + 1, p) for p in range(0,self.layers_sizes[layer + 1])])
+
+    def __layer_threshold_deltas_calculus(self):
+        self.threshold_deltas += [[self.__layer_sigma(layer,i) for i in range(self.layers_sizes[layer])] for layer in range(1,self.n_layers)]
+
+    def __adjust_thresholds(self):
+        self.u = [[None]*self.layers_sizes[0]]
+        self.u += [[self.u[i][j] - self.threshold_deltas[i][j] for j in range(self.layers_sizes[i])] for i in range(1,self.n_layers)]
+
+    def __thresholds_adjustment(self):
+        self.threshold_deltas = [[None]*self.layers_sizes[0]] # Fill the first row of threshold with None (The input thresholds).
+        self.__layer_threshold_deltas_calculus()
+        self.__adjust_thresholds()
 
     def __layer_weight_deltas_calculus(self):
         self.weight_deltas = [[[self.a[layer - 1][j] * self.__layer_sigma(layer,i) for j in range(self.layers_sizes[layer - 1])] for i in range(self.layers_sizes[layer])] for layer in range(1,self.n_layers)]
@@ -42,7 +60,7 @@ class NeuralNetwork():
 
     def __generate_random_weigths(self):
         self.w = [[[random() for j in range(self.layers_sizes[k])] for i in range(self.layers_sizes[k + 1])] for k in range(self.n_layers - 1)]
-        self.append([None]*self.layers_sizes[0])
+        self.u = [None]*self.layers_sizes[0]
         self.u += [[random() for j in range(self.layers_sizes[i])] for i in range(1, self.n_layers)]
 
     def train(self, inputs, outputs, times):
@@ -56,3 +74,30 @@ class NeuralNetwork():
                 self.__think()
                 self.__weights_adjustment()
                 self.__thresholds_adjustment()
+
+    def predict(self,input,output):
+        self.input = input
+        self.__think()
+        print("Expected: ", output)
+        print("Given: ", self.a[self.n_layers-1])
+
+if __name__ == "__main__":
+
+    train_inputs = [[0,0,0],[0,1,0],[1,0,0],[1,1,0]]
+    test_inputs = [[0,0,1],[0,1,1],[1,0,1],[1,1,1]]
+    train_outputs = [[0],[1],[1],[1]]
+    test_outputs = [[1],[1],[1],[1]]
+
+    
+    neuralNet = NeuralNetwork(
+        layers_sizes = [4,4],
+        learning_rate = 0.01
+        )
+
+    neuralNet.train(
+        train_inputs,
+        train_outputs,
+        times = 1000
+        )
+    print("Ready")
+    # neuralNetwork.predict([0,0,1],[1])
