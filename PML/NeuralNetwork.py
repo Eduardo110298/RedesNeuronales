ad48a82906639e971210cbab1e339139047fb8a2
Unknown <andreseloyluna@gmail.com>
2018-04-27 03:00:37 -0400

    Arrechisimo

--- PML/NeuralNetwork.py
+++ PML/NeuralNetwork.py
@@ -17,17 +17,6 @@ class NeuralNetwork():
         self.layers_sizes = layers_sizes
         self.learning_rate = learning_rate
 
-    def __test(self,i,j,k = 0):
-        """
-        For debug the lists comprehensions
-        """
-        print('u ',self.u,'\n')
-        print('threshold_deltas ',self.threshold_deltas,'\n')
-        print('k ',k)
-        print('i ',i)
-        print('j ',j,'\n')
-        return self.u[i][j] - self.learning_rate * self.threshold_deltas[i][j]
-
     def __sigmoid(self, x):
         return 1 / (1 + exp(-x))
     
@@ -38,22 +27,24 @@ class NeuralNetwork():
         return - (expected - given)
 
     def __layer_sigma(self,layer,i):
-        if (layer == self.n_layers - 1):
-            return self.__sigmoid_derivative(self.a[layer][i]) * self.__error_derivative(self.output[i],self.a[layer][i])
-        else:
-            return self.__sigmoid_derivative(self.a[layer][i]) * sum([self.w[layer][p][i] * self.__layer_sigma(layer + 1, p) for p in range(0,self.layers_sizes[layer + 1])])
+        return self.__sigmoid_derivative(self.a[layer][i]) * self.__error_derivative(self.output[i],self.a[layer][i]) if (layer == self.n_layers - 1) else self.__sigmoid_derivative(self.a[layer][i]) * sum([self.w[layer][p][i] * self.__layer_sigma(layer + 1, p) for p in range(0,self.layers_sizes[layer + 1])])
+
+    ########################################################################################################################################################################################################################################################################################################################
+    # Thresholds weights (u) adjustment process.
 
     def __layer_threshold_deltas_calculus(self):
         self.threshold_deltas += [[self.__layer_sigma(layer,i) for i in range(self.layers_sizes[layer])] for layer in range(1,self.n_layers)]
 
     def __adjust_thresholds(self):
         self.u = [[None]*self.layers_sizes[0]] + [[self.u[i][j] - self.learning_rate * self.threshold_deltas[i][j] for j in range(self.layers_sizes[i])] for i in range(1,self.n_layers)]
-        # self.u += [[self.test(i,j) for j in range(self.layers_sizes[i])] for i in range(1,self.n_layers)]
 
     def __thresholds_adjustment(self):
         self.threshold_deltas = [[None]*self.layers_sizes[0]] # Fill the first row of threshold with None (The input thresholds).
         self.__layer_threshold_deltas_calculus()
         self.__adjust_thresholds()
+
+    ########################################################################################################################################################################################################################################################################################################################
+    # Outputs weights (a) adjustment process.
 
     def __layer_weight_deltas_calculus(self):
         self.weight_deltas = [[[self.a[layer - 1][j] * self.__layer_sigma(layer,i) for j in range(self.layers_sizes[layer - 1])] for i in range(self.layers_sizes[layer])] for layer in range(1,self.n_layers)]
@@ -66,22 +57,59 @@ class NeuralNetwork():
         self.__layer_weight_deltas_calculus()
         self.__adjust_weights()
 
+    ########################################################################################################################################################################################################################################################################################################################
+
     def __think(self):
+        """
+        Generate the output.
+        """
         self.a = [self.input] # Initialize the output array (a), with the input array (input) in first position.
         for k in range(1,self.n_layers):
             self.a.append([self.__sigmoid(self.u[k][i] + sum([self.w[k - 1][i][j] * self.a[k - 1][j] for j in range(self.layers_sizes[k - 1])])) for i in range(self.layers_sizes[k])])
-        # self.a += [[self.__sigmoid(self.u[k][i] + sum([self.test(k,i,j) for j in range(self.layers_sizes[k - 1])])) for i in range(self.layers_sizes[k])] for k in range(1,self.n_layers)]
 
-    def __generate_random_weigths(self):
+    def __generate_random_weights(self):
+        """
+        Generate random weights for thresholds and outputs.
+        """
         self.w = [[[random() for j in range(self.layers_sizes[k])] for i in range(self.layers_sizes[k + 1])] for k in range(self.n_layers - 1)]
-        self.u = [[None]*self.layers_sizes[0]]
-        self.u += [[random() for j in range(self.layers_sizes[i])] for i in range(1, self.n_layers)]
+        self.u = [[None]*self.layers_sizes[0]] + [[random() for j in range(self.layers_sizes[i])] for i in range(1, self.n_layers)]
 
     def train(self, inputs, outputs, times):
+        """
+        Train the network.
+        inputs: is a list of study cases (inputs do not represent the outputs of the input layer, but this one: inputs[i] does represent that).
+        outputs: is a list with the output for each study case present in inputs.
+        times: represent how many times the network's weights should be adjusted using that study cases.
+
+        i.e.:
+        inputs = [
+        [0,0,0], Case 1
+        [0,1,0], Case 2
+        [1,0,0], Case 3
+        [1,1,0]  Case 4
+        ] Everyone of this inputs can be used to know how many neurons are in the input layer.
+        outputs = [
+        [0], Output for Case 1
+        [1], Output for Case 2
+        [1], Output for Case 3
+        [1]  Output for Case 4
+        ] Everyone of this output can be used to know how many neurons are in the output layer.
+
+        neuralNetwork = NeuralNetwork(
+            layers_sizes = [4,3], Two hidden layers with 4 and 3 neurons respectively.
+            learning_rate = 0.001
+            )
+
+        neuralNetwork.train(
+            inputs,
+            outputs,
+            times = 1000
+            )
+        """
         if (self.n_layers != len(self.layers_sizes)):
             self.layers_sizes = [len(inputs[0])] + self.layers_sizes + [len(outputs[0])]
-        self.__generate_random_weigths()
-        train_cases = len(inputs) # Should be len(outputs).
+        self.__generate_random_weights()
+        train_cases = len(inputs) # Could be len(outputs).
         for x in range(times):
             for i in range(train_cases):
                 self.input = inputs[i]
@@ -91,31 +119,47 @@ class NeuralNetwork():
                 self.__thresholds_adjustment()
 
     def predict(self,input,output):
+        """
+        Make an output for a specific study case.
+        input: list of outputs of input layer.
+        input: list of outputs of output layer.
+        """
         self.input = input
         if (self.n_layers != len(self.layers_sizes)):
             self.layers_sizes = [len(input)] + self.layers_sizes + [len(output)]
-            self.__generate_random_weigths()
+            self.__generate_random_weights()
         self.__think()
         print("Expected: ", output)
         print("Given: ", self.a[self.n_layers-1])
 
+    def accuracy(self,inputs,outputs):
+        """
+        Predict the output for a set of study cases.
+        inputs: list with study cases.
+        outputs: list with the output of each study case.
+        """
+        pass
+
 if __name__ == "__main__":
 
-    train_inputs = [[0,0,0],[0,1,0],[1,0,0],[1,1,0]]
+    train_inputs = [[0,0,0,0],[0,0,0,1],[0,0,1,1],[0,1,0,0],[0,1,1,1],[1,0,0,0],[1,0,1,0],[1,1,0,1]]
+    #train_inputs = [[0,0,0,0],[0,1,1,0],[1,0,1,0],[1,1,0,0],[1,1,1,0]]
     test_inputs = [[0,0,1],[0,1,1],[1,0,1],[1,1,1]]
-    train_outputs = [[0],[1],[1],[1]]
+    train_outputs = [[0],[1],[1],[1],[0],[1],[0],[0]]
     test_outputs = [[1],[1],[1],[1]]
 
-    
     neuralNet = NeuralNetwork(
-        layers_sizes = [4,4,4],
-        learning_rate = 0.001
+        layers_sizes = [3,3],
+        learning_rate = 0.1
         )
 
     neuralNet.train(
         train_inputs,
         train_outputs,
-        times = 1000
+        times = 5000
         )
     print("Ready")
-    neuralNet.predict([0,1,0],[1])
+    neuralNet.predict([0,1,1,0],[0])
+    neuralNet.predict([1,0,1,1],[0])
+    neuralNet.predict([1,1,1,0],[0])
+    neuralNet.predict([1,1,1,1],[0])
